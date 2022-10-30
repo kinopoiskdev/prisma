33e45550f61d950af6384d819c41282ae8125698
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2022-10-30 15:03:21 +0100

    remore unused files

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "./generated/prisma-client";
-
-export function createContext(ctx: any): Promise<Context>;
-
-export * from "./generated/prisma-client/index.d";
-
-export interface Context {
-    prisma: PrismaClient;
-}
